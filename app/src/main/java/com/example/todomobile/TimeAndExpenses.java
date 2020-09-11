b5e96d820d467eb5e44ad02ab0016edb830eeaca
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-09-11 11:23:43 +0200

    Create activities

---
+++ app/src/main/java/com/example/todomobile/TimeAndExpenses.java
@@ -0,0 +1,14 @@
+package com.example.todomobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimeAndExpenses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_and_expenses);
+    }
+}
