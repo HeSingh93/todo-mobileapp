e09b02fd76b61097fbfa18998db84639f5678956
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-09-16 13:12:31 +0200

    Create design for listview and implement funktion to display orderinformation in Orderlist

---
+++ app/src/main/java/com/example/todomobile/CustomListAdapter.java
@@ -0,0 +1,43 @@
+package com.example.todomobile;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.todomobile.R;
+import com.example.todomobile.models.WorkOrder;
+
+import java.util.List;
+
+public class CustomListAdapter extends ArrayAdapter {
+
+    private final Activity context;
+    private List<WorkOrder> workOrders;
+
+    public CustomListAdapter(Activity context, List<WorkOrder> workOrders) {
+        super(context, R.layout.listview_row, workOrders);
+
+        this.context = context;
+        this.workOrders = workOrders;
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.listview_row, null, true);
+
+        TextView dateTextViewID = (TextView) rowView.findViewById(R.id.dateTextViewID);
+        TextView companyNameTextViewID = (TextView) rowView.findViewById(R.id.companyNameTextViewID);
+        TextView addressTextViewID = (TextView) rowView.findViewById(R.id.addressTextviewID);
+
+        dateTextViewID.setText(workOrders.get(position).getDateTimeInfo());
+        companyNameTextViewID.setText(workOrders.get(position).getCustomer().getCompanyName());
+        addressTextViewID.setText(workOrders.get(position).getAddress());
+
+        return rowView;
+
+    }
+
+}
--- app/src/main/java/com/example/todomobile/OrderList.java
+++ app/src/main/java/com/example/todomobile/OrderList.java
@@ -2,13 +2,31 @@ package com.example.todomobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ListView;
+
+import com.example.todomobile.models.WorkOrder;
+
+import java.util.List;
 
 public class OrderList extends AppCompatActivity {
+
+    private List<WorkOrder> worksorders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_list);
+
+        Intent intent = getIntent();
+        intent.setExtrasClassLoader(WorkOrder.class.getClassLoader());
+        worksorders = intent.getParcelableArrayListExtra("WORKORDERS_LIST_MESSAGE");
+
+        ListView orderListView = findViewById(R.id.listviewID);
+        CustomListAdapter adapter = new CustomListAdapter(this, worksorders);
+        orderListView.setAdapter(adapter);
     }
 }
