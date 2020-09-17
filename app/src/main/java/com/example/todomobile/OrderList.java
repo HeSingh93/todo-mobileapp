aa6f8a2f85a5a242d3f2dfb8b2d82256aa6dd9f0
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-09-17 12:04:45 +0200

    Implement function to navigate to orderdetail from orderlist and small design changes

--- app/src/main/java/com/example/todomobile/OrderList.java
+++ app/src/main/java/com/example/todomobile/OrderList.java
@@ -5,28 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.todomobile.models.Employee;
 import com.example.todomobile.models.WorkOrder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderList extends ToDoActivity {
 
-    private List<WorkOrder> worksorders;
+    private ArrayList<WorkOrder> OrderlistActiveEmployee;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_list);
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         intent.setExtrasClassLoader(WorkOrder.class.getClassLoader());
-        worksorders = intent.getParcelableArrayListExtra(WORKORDER_LIST_MESSAGE);
+        OrderlistActiveEmployee = intent.getParcelableArrayListExtra(WORKORDER_LIST_MESSAGE);
 
-        ListView orderListView = findViewById(R.id.listviewID);
-        CustomListAdapter adapter = new CustomListAdapter(this, worksorders);
+        final ListView orderListView = findViewById(R.id.listviewID);
+        CustomListAdapter adapter = new CustomListAdapter(this, OrderlistActiveEmployee);
         orderListView.setAdapter(adapter);
+
+        orderListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(OrderList.this, OrderDetail.class);
+                intent.putExtra(CURRENT_WORKORDER_MESSAGE, (WorkOrder) parent.getItemAtPosition(position));
+                intent.putParcelableArrayListExtra(WORKORDER_LIST_MESSAGE, OrderlistActiveEmployee);
+                startActivity(intent);
+            }
+        });
     }
 }
