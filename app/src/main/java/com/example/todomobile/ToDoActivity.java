51744e456111c130c8874728eaf34f4f206083db
Karin <55209058+kodkarin@users.noreply.github.com>
2020-09-23 10:04:06 +0200

    Refactoring of constants

--- app/src/main/java/com/example/todomobile/ToDoActivity.java
+++ app/src/main/java/com/example/todomobile/ToDoActivity.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class ToDoActivity extends AppCompatActivity {
 
-    protected String WORKORDER_LIST_MESSAGE = "WORKORDER_LIST_MESSAGE";
-    protected String CURRENT_WORKORDER_MESSAGE = "CURRENT_WORKORDER_MESSAGE";
+    public static final String WORKORDER_LIST_MESSAGE = "WORKORDER_LIST_MESSAGE";
+    public static final String CURRENT_WORKORDER_MESSAGE = "CURRENT_WORKORDER_MESSAGE";
 
-    protected int STATUS_UNASSIGNED = 1;
-    protected int STATUS_ASSIGNED = 2;
-    protected int STATUS_ACCEPTED = 3;
-    protected int STATUS_DONE = 4;
+    public static final int STATUS_UNASSIGNED = 1;
+    public static final int STATUS_ASSIGNED = 2;
+    public static final int STATUS_ACCEPTED = 3;
+    public static final int STATUS_DONE = 4;
 
 }
